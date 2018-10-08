272581b0aa22eddc9deb66ea498afb0d4ca77b91
Hanner Andrés Ortiz Ramirez <ha.ortiz11@uniandes.edu.co>
2018-10-07 21:22:45 -0500

    Color Rojo : Test Editar datos Independiente

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -43,7 +43,7 @@ class FunctionalTest(TestCase):
         imagen.send_keys('D:\Imagenes\developer.jpg')
 
         nombreUsuario = self.browser.find_element_by_id('id_username')
-        nombreUsuario.send_keys('juan650')
+        nombreUsuario.send_keys('juan651')
 
         clave = self.browser.find_element_by_id('id_password')
         clave.send_keys('clave123')
@@ -76,3 +76,32 @@ class FunctionalTest(TestCase):
         ingresar.click()
         self.browser.implicitly_wait(3)
         logout = self.browser.find_element_by_id('id_logout')
+
+    def test_editar(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_editar')
+        link.click()
+
+        link = self.browser.find_element_by_id('id_login')
+        link.click()
+
+        # login usuario
+        username = self.browser.find_element_by_id('id_login_username')
+        username.send_keys('juan648')
+        password = self.browser.find_element_by_id('id_login_password')
+        password.send_keys('clave123')
+
+        self.browser.implicitly_wait(3)
+        logout = self.browser.find_element_by_id('id_logout')
+
+        #Editar
+        self.browser.implicitly_wait(3)
+        ingresar = self.browser.find_element_by_id('id_ingresar')
+        ingresar.click()
+        nombre = self.browser.find_element_by_id('id_nombre')
+        nombre.send_keys('Raul Andres')
+        botonGrabar = self.browser.find_element_by_id('id_grabar')
+        botonGrabar.click()
+        self.browser.implicitly_wait(3)
+        span = self.browser.find_element(By.XPATH, '//span[text()="Jose Andres Ortiz"]')
+        self.assertIn('Jose Andres Ortiz', span.text)
